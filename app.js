68c104f4339392864f2bdc11cae0ef1f84316221
AZ788 <j60356446@gmail.com>
2024-10-14 08:13:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const nodemailer = require('nodemailer');
 const routes = require('./routes/auth');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Configuración de la base de datos
 const db = mysql.createConnection({

d83b251d1ffb630017f9f92344c2777d76761af5
AZ788 <j60356446@gmail.com>
2024-10-14 14:49:24 -0500

    Update appp.js

--- appp.js
+++ appp.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const app = express();
-const port = 5000;
+const port = 6000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
